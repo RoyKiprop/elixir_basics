b165f7fd74d34a02322706cb94adb4674bcf5d0e
Roy Kiprop <roykiprop590@gmail.com>
2024-09-17 09:40:41 +0300

    Add unique products filter module

--- lib/Unique_Products.ex
+++ lib/Unique_Products.ex
@@ -1,3 +1,19 @@
 defmodule Unique_Products do
 
+  @products [
+    %{name: "apple", price: 1.2},
+    %{name: "banana", price: 0.5},
+    %{name: "apple", price: 1.3},
+    %{name: "orange", price: 0.8}
+  ]
+
+  def unique_products do
+
+    @products
+    |> Enum.map(&(&1[:name]))
+    |> Enum.uniq()
+  end
+
+
+
 end
