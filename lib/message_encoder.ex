c3a4dc1a8eb83b184df4b66f1143ebfd2c3b17ee
Roy Kiprop <roykiprop590@gmail.com>
2024-09-17 11:06:54 +0300

    Add message encoder module

---
+++ lib/message_encoder.ex
@@ -0,0 +1,24 @@
+defmodule MessageEncoder do
+  @alphabet ?a..?z
+
+  def letter_position do
+    @alphabet
+    |> Enum.to_list()
+    |> Enum.map(fn letter -> {<<letter::utf8>> , letter - ?a + 1} end)
+    |> Enum.into(%{})
+  end
+
+  def encode(message) do
+    letter_map = letter_position()
+
+    message
+    |> String.downcase()
+    |> String.graphemes()
+    |> Enum.map(fn letter ->
+
+      Map.get(letter_map, letter )
+
+    end)
+
+  end
+end
