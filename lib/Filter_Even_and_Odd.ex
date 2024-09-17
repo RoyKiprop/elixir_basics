ef92d2ff60e2c54b41c4ce4a3d72e0fdbf18bacf
Roy Kiprop <roykiprop590@gmail.com>
2024-09-17 05:46:17 +0300

    Add filter even and odd module

--- lib/Filter_Even_and_Odd.ex
+++ lib/Filter_Even_and_Odd.ex
@@ -1,3 +1,15 @@
 defmodule Filter_EvenAnd_Odd do
 
+  def split_even_odd(numbers) do
+     Enum.reduce(numbers, {[], []}, fn number, {even, odd} ->
+      if rem(number, 2) == 0 do
+        {[number | even], odd}
+      else
+        {even, [number | odd ]}
+      end
+     end)
+     |> Tuple.to_list()
+     |> Enum.map(fn x -> Enum.reverse(x) end)
+     |> List.to_tuple()
+  end
 end
